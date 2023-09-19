dc7e97335cb9692795651f0028b7579e03be8064
ziuge <xwcjwwd@ewhain.net>
2023-09-19 19:11:07 +0900

    Initial Commit

---
+++ PracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by CHOI on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
